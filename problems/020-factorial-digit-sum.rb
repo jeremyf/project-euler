36f67800f3a06ba8abe696a1527a1d5bd57b03b9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-10-17 18:47:49 -0400

    Solving #20, factorial digit sum

---
+++ problems/020-factorial-digit-sum.rb
@@ -0,0 +1,8 @@
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+#
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#
+# Find the sum of the digits in the number 100!
+# @see https://projecteuler.net/problem=20
+puts (1..100).reduce(1, :*).to_s.split("").reduce(0) {|sum, i| sum + i.to_i }
